96f44d7ac520ee2b19bc451acdfabdc7e1b82c39
Iago Pinheiro <130023096+iagompinheiro@users.noreply.github.com>
2023-09-30 19:12:30 -0300

    create ActivityService

---
+++ src/main/java/com/iagompinheiro/backend/BarberShop/service/ActivityService.java
@@ -0,0 +1,58 @@
+package com.iagompinheiro.backend.BarberShop.service;
+
+import com.iagompinheiro.backend.BarberShop.domain.domainActivity.Activity;
+import com.iagompinheiro.backend.BarberShop.dto.ActivityDto;
+import com.iagompinheiro.backend.BarberShop.repositories.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+public class ActivityService {
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    public Activity create(ActivityDto activityDto) {
+        Activity newActivity = new Activity(activityDto);
+        switch (newActivity.getActivityType()) {
+            case LOWFADE:
+                newActivity.setValueType(30.0);
+                break;
+            case MIDFADE:
+                newActivity.setValueType(35.0);
+                break;
+            case AMERICAN:
+                newActivity.setValueType(40.0);
+                break;
+            case HIGHFADE:
+                newActivity.setValueType(25.0);
+            default:
+                break;
+        }
+        return activityRepository.save(newActivity);
+    }
+
+    public List<Activity> getAll() {
+       return activityRepository.findAll();
+    }
+
+    public Activity activityById(Long id) {
+        return activityRepository.findById(id).get();
+    }
+
+    public void deleteById(Long id) {
+        activityRepository.deleteById(id);
+    }
+
+    public Activity update(Long id, ActivityDto activityDto) {
+        Activity updateActivity = activityById(id);
+        updateActivity.setActivityType(activityDto.activityType());
+        updateActivity.setDateTime(activityDto.dateTime());
+        activityRepository.save(updateActivity);
+        return updateActivity;
+    }
+
+
+}
